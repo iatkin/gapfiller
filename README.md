6e0455e74750d0773f9e34c71507c850a7292848
Devin Wild Thomas <dwt@cs.unh.edu>
2026-04-30 13:51:49 -0400

    py - updated to use the 3d model beam

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Install Anaconda
 conda env create --name gapfiller --file=gapfiller_env.yml
 sudo apt install libboost-all-dev cmake ninja-build build-essential libgdal-dev
+
+pip install manifold3d trimesh pyvista
--- gapfiller_helper.py
+++ gapfiller_helper.py
@@ -95,6 +95,6 @@ if __name__ == "__main__":
             crs=wgs84,
         )
         if swath:
-            swath_gdf = m.survey_line(output_gdf)
+            swath_gdf = m.survey_line_3D(output_gdf)
             output_gdf = gpd.GeoDataFrame(pd.concat([output_gdf, swath_gdf[0]], ignore_index=True), crs = output_gdf.crs)
         print(output_gdf.to_json())    
